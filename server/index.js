4ad16137ac445226daf9f87b7d39a4ec722472fb
kemkengfowo-cloud <kemkengfowo@@gmail.com>
2026-09-04 13:02:52 +0200

    fix server.listen apres connexion MongoDB

--- server/index.js
+++ server/index.js
@@ -58,7 +58,7 @@ app.use("/api/admin", require("./routes/admin"));
 
 app.get("/api/health", (req, res) => res.json({ status: "OK", message: "B.Y.H API running" }));
 mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/batilink", { serverSelectionTimeoutMS: 30000, connectTimeoutMS: 30000, socketTimeoutMS: 30000 })
-  .then(async () => { console.log("MongoDB connecte"); const { createIndexes } = require("./utils/createIndexes"); await createIndexes(); require("./utils/cronJobs"); })
+  .then(async () => { console.log("MongoDB connecte"); const { createIndexes } = require("./utils/createIndexes"); await createIndexes(); require("./utils/cronJobs"); server.listen(PORT, () => console.log("Serveur B.Y.H sur port " + PORT)); })
   .catch(err => console.error("Erreur MongoDB:", err));
 
 const { lancerTousLesRappels } = require("./utils/rappels");
@@ -72,7 +72,6 @@ const { initSocket } = require("./socket");
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 initSocket(server);
-server.listen(PORT, () => console.log("Serveur B.Y.H sur port " + PORT));
 
 
 
